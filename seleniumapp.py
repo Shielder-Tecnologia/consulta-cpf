27331bacd3af349643e95e24896770e417fcc785
Arigol <antonio.rigolino@gmail.com>
2022-03-28 11:57:25 -0300

    Implementando random user agent para evitar bloqueio por parte do site

--- seleniumapp.py
+++ seleniumapp.py
@@ -1,14 +1,22 @@
+from wsgiref.simple_server import software_version
 import fordev as fd
 import os
+from selenium import webdriver
 from selenium.webdriver.firefox.options import Options
 from selenium.webdriver.common.by import By
 from selenium.webdriver import Firefox
+from random_user_agent.user_agent import UserAgent
+from random_user_agent.params import SoftwareName, OperatingSystem
 
 #### SETUP #####
+soft_names = [SoftwareName.FIREFOX.value]
+op_sys = [OperatingSystem.LINUX.value,OperatingSystem.WINDOWS.value]
+user_agent_rotator = UserAgent(software_names=soft_names,operating_systems=op_sys,limit=100)
+usuario = user_agent_rotator.get_random_user_agent()
 opt = Options()
 opt.add_argument("--headless")
+opt.add_argument(f'user-agent={usuario}')
 browser = Firefox(options=opt)
-browser.get("https://www.situacao-cadastral.com/")
 
 path = os.getcwd()
 
@@ -16,6 +24,7 @@ print("Insira o cpf a ser procurado: ")
 cpf = input()
 
 if fd.validators.is_valid_cpf(cpf):
+    browser.get("https://www.situacao-cadastral.com/")
     campo_consulta = browser.find_element(By.ID,"doc")
     campo_consulta.send_keys(cpf)
     browser.find_element(By.ID,"consultar").click()
