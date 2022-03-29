007f477b2c19926b9a4abfae8ab1557c538fd743
Arigol <antonio.rigolino@gmail.com>
2022-03-29 16:49:04 -0300

    Removendo dependencia fordev

--- seleniumapp.py
+++ seleniumapp.py
@@ -1,4 +1,3 @@
-import fordev as fd
 import os
 from selenium.webdriver.firefox.options import Options
 from selenium.webdriver.common.by import By
@@ -21,21 +20,19 @@ path = os.getcwd()
 print("Insira o cpf a ser procurado: ")
 cpf = input()
 
-if fd.validators.is_valid_cpf(cpf):
-    browser.get("https://www.situacao-cadastral.com/")
-    campo_consulta = browser.find_element(By.ID,"doc")
-    campo_consulta.send_keys(cpf)
-    browser.find_element(By.ID,"consultar").click()
-    try:
-        result = browser.find_element(By.ID,"resultado")
-        # Caso queira uma screenshot do resultado da página descomentar
-        #result.screenshot(path + '/result.png')
-        print('\n' + result.text)
-    except Exception as erro:
-        print("CPF ainda não cadastrado ou erro na consulta")
-    finally:
-        browser.quit()
+browser.get("https://www.situacao-cadastral.com/")
+campo_consulta = browser.find_element(By.ID,"doc")
+campo_consulta.send_keys(cpf)
+browser.find_element(By.ID,"consultar").click()
+try:
+    result = browser.find_element(By.ID,"resultado")
+    # Caso queira uma screenshot do resultado da página descomentar
+    #result.screenshot(path + '/result.png')
+    print('\n' + result.text)
+except Exception as erro:
+    print("CPF ainda não cadastrado ou erro na consulta")
+finally:
+    browser.quit()
 
 
-else:
-    print("CPF INVÁLIDO")
+
